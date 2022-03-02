e57d2b2e1434ea34b6e1df2678c04e04002c8627
Евгений Пермяков <evgenyu.permyakov@yandex.ru>
2022-03-02 19:31:43 +0500

    Commit 2

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -21,6 +21,5 @@ class Post(models.Model):
         on_delete=models.CASCADE,
         related_name='posts'
     )
-    group = models.ForeignKey(Group, on_delete=models.CASCADE, blank=True, null=True)
-
-
+    group = models.ForeignKey(Group, on_delete=models.CASCADE,
+                              blank=True, null=True)
